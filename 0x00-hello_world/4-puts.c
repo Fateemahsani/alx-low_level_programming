61f1c1b93660d7e1c2a051423fe8146954ab3ebc
fatima sani <fatima sani>
2022-06-30 21:43:47 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #!/bin/bash
-#include<stdio.h>
-
+#include <stdio.h>
 /**
+ * main -enter point
  *
- * * main -entry point
- *
- * * Return: Always 0 (sucess)
- *
- * */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+	return(0);
 }
