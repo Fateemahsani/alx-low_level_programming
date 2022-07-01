b62dacfa3c3c54ac61d3c7fde33da72aa913727d
fatima sani <fatima sani>
2022-07-01 16:02:59 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
  * main -> assign a random number to the variable
  * n each time it is executed
  * Description: postive_or_negative
- * Return : always 0
+ * Return: Always 0
  */
 int main(void)
 {
