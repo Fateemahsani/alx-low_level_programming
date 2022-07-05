c1617fbfc4d0d524002abdbbe1ef2229d441ec29
fatima sani <fatima sani>
2022-07-04 21:35:34 -0700

    hello print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
-#!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main -entry point
  * Return: Always 0 (sucess)
@@ -7,5 +6,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
